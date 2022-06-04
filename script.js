98b085bc7dd4aa8de947ffbb2826b51d45ad631b
gih-costa <gih.costa123@hotmail.com>
2022-06-04 15:26:32 -0300

    feat 03 - modificação area de projetos

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const btnMobile = document.getElementById('btn-mobile');
+// const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu() {
-  const nav = document.getElementById('nav');
-  nav.classList.toggle('active')
-}
+// function toggleMenu() {
+//   const nav = document.getElementById('nav');
+//   nav.classList.toggle('active')
+// }
 
-btnMobile.addEventListener('click', toggleMenu)
+// btnMobile.addEventListener('click', toggleMenu)
